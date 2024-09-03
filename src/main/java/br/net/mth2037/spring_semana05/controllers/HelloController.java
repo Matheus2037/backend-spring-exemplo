8a47443a22e2bc856f12cf13961ae46d943f75a8
Matheus Da Silva Gastaldi <matheus.30220@alunosatc.edu.br>
2024-09-02 21:02:25 -0300

    Segundo commit

--- src/main/java/br/net/mth2037/spring_semana05/controllers/HelloController.java
+++ src/main/java/br/net/mth2037/spring_semana05/controllers/HelloController.java
@@ -2,12 +2,16 @@ package br.net.mth2037.spring_semana05.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-
+    @GetMapping("/olaAluno")
+    public String olaAluno(@RequestParam String aluno){
+        return "Ola "+ aluno +"!";
+    }
     @GetMapping("/olaMundo")
     public String olaMundo(){
         return "Ola, Mundo!";
